71c1527df8c7e607988b4711e67397f58042d4cd
you@example.com <you@example.com>
2019-12-11 18:32:31 +0900

    Загрузчик страниц

--- hello/views.py
+++ hello/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import requests   ######################################
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
@@ -17,3 +19,16 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+
+def page(request):
+    url = request.GET.get('url', "")
+    print("ПОСТУПИЛ URL:", url)
+    if not url:  ################################################
+        return HttpResponse('<form><input type="text" name="url"><input type="submit"></form>')
+    else:                               ######
+        r = requests.get(url)           ######
+        result = 'ERROR'                ######
+        if r.status_code == 200:        ######
+            result = r.content          ######
+        return HttpResponse(result)     ######
